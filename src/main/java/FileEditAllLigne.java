83773c65061a57718041bd559b755d805d13b9d3
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-03-18 17:04:17 +0100

    c

--- src/main/java/FileEditAllLigne.java
+++ src/main/java/FileEditAllLigne.java
@@ -25,15 +25,85 @@ public class FileEditAllLigne extends CommandFileEdit {
                 maxSize = str.length();
             }
         }
-        WindowF w = new WindowF("FileEdit", maxSize * 15, strings.length * 25);
+        WindowF w = new WindowF("FileEdit", maxSize * 25, strings.length * 40) {
+            @Override
+            public void waitClosed() {
+                int maxLine = jtf.getText().split("\n").length;
+                while (wait) {
+                    String text = jtf.getText();
+                    String[] lines = text.split("\n");
+                    int pos = jtf.getCaretPosition();
+                    int lineI = getLine(text, pos);
+                    String line = lines[lineI];
+                    if (maxLine < lines.length) {
+                        if (text.endsWith("{\n")) {
+                            int n = 0;
+                            char c = line.charAt(0);
+                            StringBuilder pre = new StringBuilder();
+                            while (c == ' ') {
+                                n++;
+                                pre.append(" ");
+                                c = line.charAt(n);
+                                if (n == line.length()-1) {
+                                    c = 'a';
+                                }
+                            }
+                            String test = pre + "      \n" + pre + "}";
+                            jtf.insert(test, getEndLine(text, lineI));
+                            jtf.setCaretPosition(getEndLine(text, lineI));
+                        }else if (text.endsWith("\n")) {
+                            int n = 0;
+                            char c = line.charAt(0);
+                            StringBuilder pre = new StringBuilder();
+                            while (c == ' ') {
+                                n++;
+                                pre.append(" ");
+                                c = line.charAt(n);
+                                if (n == line.length()-1) {
+                                    c = 'a';
+                                }
+                            }
+                            jtf.append(String.valueOf(pre));
+                        }
+                    }else{
+                        maxLine = lines.length;
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
         jtf.setForeground(Color.WHITE);
         jtf.setBackground(Color.BLACK);
         JPanel top = new JPanel();
         top.setBackground(Color.BLACK);
         top.add(jtf);
-        w.container.add(top, BorderLayout.NORTH);
+        w.container.add(top, BorderLayout.WEST);
         w.setContentPane(w.container);
         w.setVisible(true);
         w.waitClosed();
+    }
+
+    public static int getLine(String text, int pos) {
+        String[] lines = text.split("\n");
+        int n = 0;
+        for (int i = 0; i < lines.length; i++) {
+            if (pos > n) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static int getEndLine(String text, int line) {
+        String[] lines = text.split("\n");
+        int n = 0;
+        for (int i = 0; i < line + 1; i++) {
+            n += lines[line].length();
+        }
+        return n;
     }
 }
--- src/main/java/WindowF.java
+++ src/main/java/WindowF.java
@@ -1,12 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.util.HashMap;
 
-public class WindowF extends JFrame implements WindowListener {
+public abstract class WindowF extends JFrame {
     public JPanel container = new JPanel();
-    public static HashMap<String, Boolean> windows = new HashMap<>();
+    public Boolean wait = true;
 
     public WindowF(String name, int x, int y) {
         this.setTitle(name);
@@ -16,55 +13,13 @@ public class WindowF extends JFrame implements WindowListener {
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         container.setBackground(Color.BLACK);
         container.setLayout(new BorderLayout());
-        windows.put(name, true);
-    }
-
-    @Override
-    public void windowOpened(WindowEvent e) {
-        System.out.println("Window open: " + e.getWindow().getName());
-    }
-
-    @Override
-    public void windowClosing(WindowEvent e) {
-        System.out.println("Window closing: " + e.getWindow().getName());
-    }
-
-    @Override
-    public void windowClosed(WindowEvent e) {
-        System.out.println("Window closed: " + e.getWindow().getName());
-        windows.replace(e.getWindow().getName(), false);
-    }
-
-    @Override
-    public void windowIconified(WindowEvent e) {
-        System.out.println("I am a cake");
-    }
-
-    @Override
-    public void windowDeiconified(WindowEvent e) {
-        System.out.println("I am a Minecraft server");
-    }
-
-    @Override
-    public void windowActivated(WindowEvent e) {
-        System.out.println("I am a Discord server");
-    }
-
-    @Override
-    public void windowDeactivated(WindowEvent e) {
-        System.out.println("I am a ... I don't know");
-    }
-
-    public void waitClosed() {
-        int i = 100; //pour éviter de manger tout le processeur
-        boolean test = true;
-        while (test) {
-            i--;
-            if (i == 0) {
-                test = windows.get(this.getName());
-                i = 100;
+        this.addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                wait = false;
             }
-        }
-        windows.remove(this.getName());
+        });
     }
+
+    public abstract void waitClosed();
 }
