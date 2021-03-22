03a0c56ade898ae26defce6e98afde32cc81be04
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-03-22 16:45:07 +0100

    c

--- src/main/java/WriteHelp.java
+++ src/main/java/WriteHelp.java
@@ -6,6 +6,6 @@ public class WriteHelp extends CommandFileEdit {
 
     @Override
     public void execute(String[] args, String command, File file, FileEditApp app, Admin admin) {
-        System.out.println("Use >> to write a lign, >>clear to clear, >>feal to edit all content");
+        System.out.println("Use >> to write a lign, >>clear to clear, >>feal to edit all content, >>fral : replace line per line content of the file");
     }
 }
